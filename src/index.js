a1581528d4113a50ca1fbe91f9bde02894384bb4
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-17 12:43:01 -0400

    added and deleted some useless code :)

--- src/index.js
+++ src/index.js
@@ -8,41 +8,18 @@ document.addEventListener('DOMContentLoaded', (e)=>{
     e.preventDefault()
     // const menu = document.getElementById('raman-menu')
 
-fetch(ramanUrl)
+addImages()
+})        
+
+function addImages(){
+return fetch(ramanUrl)
     .then(res => res.json())
     // .then(data => console.log(data))
     .then(image => image.forEach(image => {
-        const imageCard = document.createElement('img');
-        imageCard.src = 'image.image';    
-        menu.appendChild(imageCard)
+        const imageCard = document.createElement('div');
+        const actualImage = document.createElement('img')
+        actualImage.src = `${image.image}`;    
+        menu.appendChild(imageCard).appendChild(actualImage)
     }))
-})        
-
-let form = document.querySelector('form')
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    createNewRaman(e.target.value);
-    form.reset();
-})
-
-function createNewRaman(raman){
-    let newRam = document.createElement('div')
-    let p1 = document.createElement('p')
-    let p2 = document.createElement('p')
-    let img = document.createElement('img')
-    let p4 = document.createElement('p')
-    let p5 = document.createElement('p')
 }
-// const configurationObject = {
-//     method: "POST",
-//     headers: {
-//         'Content-Type': 'application/json',
-//         Accept: 'application/json'
-//     },
-//     body: JSON.stringify({
 
-//     })
-// }
-
-
-///were so freaking close Jeremey look how far youve come. So we managedd to run through the images and display all 5 images, however we werent able to pull the image src from the data so the images displayed were nothing. So go back and figure out exactly how to pinpoint those. You spent 99% of your time on this lab trying to figure that out. Use better time management! Youre almost there though!
